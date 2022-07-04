5f4f53de37865484eecfa4af5a77669b517ea54b
Jongwon <per1215@naver.com>
2022-07-05 00:02:23 +0900

    📦 styled-component 설치

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {BrowserRouter} from 'react-router-dom'
 import App from './App';
 
 
@@ -8,7 +8,8 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <BrowserRouter>
     <App />
+    </BrowserRouter>
 );
-
 
