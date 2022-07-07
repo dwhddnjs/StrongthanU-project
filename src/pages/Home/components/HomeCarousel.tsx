2d242a4612a4ffa5c93b0a9375bc73e4a419048e
Jongwon <per1215@naver.com>
2022-07-08 01:00:39 +0900

    🔥 react-slick을 제거 하고 직접 구현합니다

--- src/pages/Home/components/HomeCarousel.tsx
+++ src/pages/Home/components/HomeCarousel.tsx
@@ -1,70 +1,86 @@
-import React from 'react';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Slider from 'react-slick';
+import React, { useState } from 'react';
 import dead from '../../../images/dead.jpg';
 import squat from '../../../images/squat.jpg';
 import ohp from '../../../images/ohp.jpg';
 import bench from '../../../images/bench.jpg';
 import styled from 'styled-components';
+import { BiRightArrow, BiLeftArrow } from 'react-icons/bi';
+
+const imgData = [
+  {
+    id: 1,
+    image: squat,
+  },
+  {
+    id: 2,
+    image: bench,
+  },
+  {
+    id: 3,
+    image: dead,
+  },
+  {
+    id: 4,
+    image: ohp,
+  },
+];
 
 const HomeCarousel = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoPlay: true,
-    autoplaySpeed: 1000,
-
-    // slide: 'div',
-    arrows: true,
-    draggable: true,
-    prevArrow: <button type="button">Previous</button>,
-    nextArrow: <button type="button">Next</button>,
-  };
+  const [current, setCurrent] = useState(1);
 
   return (
-    <Slider {...settings}>
-      <CarouselItem>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum aut, dolorum cumque totam odio nam ea natus
-          voluptatibus, excepturi eligendi nostrum adipisci pariatur, corporis enim aliquid nesciunt eaque molestiae
-          amet?
-        </p>
-        <img src={squat} alt="" width="800px" />
-      </CarouselItem>
-      <CarouselItem>
-        <img src={bench} alt="" width="800px" />
-      </CarouselItem>
-      <CarouselItem>
-        <img src={dead} alt="" width="800px" />
-      </CarouselItem>
-      <CarouselItem>
-        <img src={ohp} alt="" width="800px" />
-      </CarouselItem>
-    </Slider>
+    <CarouselContainer>
+      <CarouselArrowWrapper>
+        <CarouselButton>
+          <BiLeftArrow className="prev" />
+        </CarouselButton>
+        {imgData.map(
+          (img) =>
+            current === img.id && (
+              <CarouselItem key={img.id}>
+                <img src={img.image} alt="" width={800} height={550} />
+              </CarouselItem>
+            ),
+        )}
+        <CarouselButton>
+          <BiRightArrow className="next" />
+        </CarouselButton>
+      </CarouselArrowWrapper>
+    </CarouselContainer>
   );
 };
 
 export default HomeCarousel;
+
 const CarouselContainer = styled.div`
-  display: flex;
   border: 1px solid;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  height: 1000px;
+  display: flex;
+  justify-content: end;
+  padding: 80px;
 `;
 
 const CarouselItem = styled.div`
-  /* max-width: fit-content; */
-  border: 1px solid;
-  justify-content: center;
+  width: 800px;
+  height: 550px;
+  display: flex;
   align-items: center;
+  justify-content: space-between;
+  font-size: 50px;
+  color: #ffffff;
+`;
+
+const CarouselArrowWrapper = styled.div`
+  height: fit-content;
+  display: flex;
+  align-items: center;
+  font-size: 50px;
+  color: #333333;
   border-radius: 10px;
-  margin: 100;
   img {
     border-radius: 10px;
+    box-shadow: rgba(0, 0, 0, 0.3) 0px 40px 80px, rgba(0, 0, 0, 0.22) 0px 30px 24px;
   }
 `;
+
+const CarouselButton = styled.div``;
---
+++ src/pages/Home/components/HomeCarouselButton.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { IoIosArrowBack } from 'react-icons/io';
+
+const HomeCarouselButton = () => {
+  return <IoIosArrowBack />;
+};
+
+export default HomeCarouselButton;
