bbc0d59f4238f06b6c09f8664091b10619cab0db
Jongwon <per1215@naver.com>
2022-08-10 01:20:54 +0900

    🎨 api url 변경합니다

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://strongthanu-api.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
